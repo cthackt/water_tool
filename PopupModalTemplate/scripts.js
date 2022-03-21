3b441f4fdf3ac3b58e5e0460081dd2ef381d7cbd
Caspian Thackeray <cthackt@NotATrashCan.local>
2022-03-21 12:36:17 -0700

    started modal template

---
+++ PopupModalTemplate/scripts.js
@@ -0,0 +1,11 @@
+// show modal
+document.querySelector("#modal-button").addEventListener("click", ()=>{
+    document.querySelector("#myModal").classList.toggle("hide");
+    document.querySelector("#overlay").classList.toggle("hide");
+});
+
+// show modal
+document.querySelector("#hide-modal").addEventListener("click", ()=>{
+    document.querySelector("#myModal").classList.toggle("hide");
+    document.querySelector("#overlay").classList.toggle("hide");
+});
